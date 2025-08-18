8359d1f4a9764ac6c486f2013c74c12503d89ea4
Kingsman102bheng <abrehanluisadrian315@gmail.com>
2025-08-18 15:51:35 +0800

    initial commit

---
+++ activity9.py
@@ -0,0 +1,11 @@
+a = 120
+b = 120
+
+c = 9
+d = 8
+username = 'abrehan'
+password = 'hulaan_mo' 
+print((username == 'abrehan' ) and (password == 'hulaan_mo'))
+print( (a > b) and (c < d) ) 
+print( (a > b) or (c < d) ) 
+print(not((a > b) or (c < d))) 
